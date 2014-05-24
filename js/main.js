9cfc0b4ccce33eeea4b00c5c87713ec718b5426e
almossawi <almossawi@gmail.com>
2014-05-23 21:41:05 -0700

    Fixes issue #72

--- js/main.js
+++ js/main.js
@@ -470,4 +470,36 @@ $(document).ready(function() {
             })
         })
     }
+    
+
+    //replace all SVG images with inline SVG
+    //http://stackoverflow.com/questions/11978995/how-to-change-color-of-svg
+    //-image-using-css-jquery-svg-image-replacement
+    $('img.svg').each(function() {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+    });
 })
