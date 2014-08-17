564d997ec57fe88832abf890a6a1068479cfca98
Anthony LaTorre <tlatorre@uchicago.edu>
2014-08-16 20:34:56 -0500

    don't set min_x to 0 if min(x) > 0

--- js/metrics-graphics.js
+++ js/metrics-graphics.js
@@ -172,9 +172,7 @@ function xAxis(args) {
 
     args.x_axis_negative = false;
     if (!args.time_series) {
-        if (min_x >= 0){
-            min_x = 0;
-        } else  {
+        if (min_x < 0){
             min_x = min_x  - (max_x * (args.inflator-1));
             args.x_axis_negative = true;
         }
