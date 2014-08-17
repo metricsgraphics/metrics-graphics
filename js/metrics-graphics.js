cfbcbfc871eb5509f2b4f212c7c99a45bf846a41
Anthony LaTorre <tlatorre@uchicago.edu>
2014-08-16 20:29:20 -0500

    convert string -> number when binning histogram data

--- js/metrics-graphics.js
+++ js/metrics-graphics.js
@@ -534,6 +534,9 @@ function process_histogram(args){
         } else if (typeof(our_data[0]) == 'number'){
             // we are dealing with a simple array of numbers. No extraction needed.
             extracted_data = our_data;
+        } else if (typeof(our_data[0]) == 'string'){
+            // convert string -> number
+            extracted_data = our_data.map(function(d) { return +d; });
         }
         var hist = d3.layout.histogram()
         if (args.bins){
