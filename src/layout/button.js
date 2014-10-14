bef66b85c0876dc04510ea83294dc0e1def12fc5
almossawi <almossawi@gmail.com>
2014-10-14 14:06:59 -0700

    Made style consistent with rest of codebase

--- src/layout/button.js
+++ src/layout/button.js
@@ -7,20 +7,19 @@ var button_layout = function(target) {
     this.manual_map = {};
     this.manual_callback = {};
 
-    this.data = function(data){
+    this.data = function(data) {
         this._data = data;
         return this;
     }
 
-    this.manual_button = function(feature, feature_set, callback){
+    this.manual_button = function(feature, feature_set, callback) {
         this.feature_set[feature]=feature_set;
         this.manual_map[strip_punctuation(feature)] = feature;
         this.manual_callback[feature]=callback;// the default is going to be the first feature.
         return this;
     }
 
-
-    this.button = function(feature){
+    this.button = function(feature) {
         var sorter, the_label;
         if (arguments.length>1) {
             this.public_name[feature] = arguments[1];
@@ -33,38 +32,40 @@ var button_layout = function(target) {
         return this;
     }
 
-    this.callback = function(callback){
+    this.callback = function(callback) {
         this._callback = callback;
         return this;
     }
 
-    this.display = function(){
+    this.display = function() {
         var callback = this._callback;
         var manual_callback = this.manual_callback;
         var manual_map = this.manual_map;
 
         var d,f, features, feat;
         features = Object.keys(this.feature_set);
+        
         // build out this.feature_set with this.data.
-        for (var i=0; i < this._data.length; i++){
+        for (var i=0; i < this._data.length; i++) {
             d = this._data[i];
-            f = features.map(function(f){return d[f]});
-            for (var j=0;j<features.length;j++){
+            f = features.map(function(f) {return d[f]});
+            for (var j=0;j<features.length;j++) {
                 feat=features[j]; 
-                //if (!this.feature_set.hasOwnProperty(feat))     this.feature_set[feat]=[];
                 if (this.feature_set[feat].indexOf(f[j])==-1)   this.feature_set[feat].push(f[j]);
             }
         }
-        for (var feat in this.feature_set){
-            if (this.sorters.hasOwnProperty(feat)){
+        for (var feat in this.feature_set) {
+            if (this.sorters.hasOwnProperty(feat)) {
                 this.feature_set[feat].sort(this.sorters[feat]);
             }
         }
+
         $(this.target).empty();
         
         $(this.target).append("<div class='col-lg-12 segments text-center'></div>");
-        var the_string='';
-        for (var feature in this.feature_set){
+        var the_string = '';
+
+        for (var feature in this.feature_set) {
             features = this.feature_set[feature];
             $(this.target + ' div.segments').append(
                     '<div class="btn-group '+strip_punctuation(feature)+'-btns text-left">' + // This never changes.
@@ -78,20 +79,22 @@ var button_layout = function(target) {
                         (!this.manual_callback.hasOwnProperty(feature) ? '<li class="divider"></li>' : "") +
                     '</ul>'
             + '</div>');
-            for (var i=0;i<features.length;i++){
-                if (features[i] != 'all' && features[i]!=undefined){ // strange bug with undefined being added to manual buttons.
+
+            for (var i=0;i<features.length;i++) {
+                if (features[i] != 'all' && features[i]!=undefined) { // strange bug with undefined being added to manual buttons.
                     $(this.target + ' div.' + strip_punctuation(feature) + '-btns ul.dropdown-menu').append(
                     '<li><a href="#" data-feature="' + strip_punctuation(feature) + '" data-key="' + features[i] + '">' 
                         + features[i] + '</a></li>'
                     ); 
                 }
-            };
-            $('.'+ strip_punctuation(feature) + '-btns .dropdown-menu li a').on('click', function() {
+            }
+
+            $('.' + strip_punctuation(feature) + '-btns .dropdown-menu li a').on('click', function() {
                 var k = $(this).data('key'); 
                 var feature = $(this).data('feature');
                 var manual_feature;
                 $('.' + strip_punctuation(feature) + '-btns button.btn span.title').html(k);
-                if (!manual_map.hasOwnProperty(feature)){
+                if (!manual_map.hasOwnProperty(feature)) {
                     callback(feature, k);    
                 } else {
                     manual_feature = manual_map[feature];
