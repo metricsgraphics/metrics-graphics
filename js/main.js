6c2ae5f1ca3919144524c03d8dcec349c437fa2a
almossawi <almossawi@gmail.com>
2014-10-21 17:07:25 -0700

    Fixed spelling mistake in chart description

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
         //add a line chart that has a few observations
         moz_chart({
             title: "Few Observations",
-            description: "We sometimes have only a few observations. By setting <i>missing_is_zero: true</i>, missing values for a time-series will be interpreted as zeros. In this example, we've override the rollover callback to show 'no date' for missing observations and have set <i>min_x</i> and <i>max_x</i> to dates beyond our date range.",
+            description: "We sometimes have only a few observations. By setting <i>missing_is_zero: true</i>, missing values for a time-series will be interpreted as zeros. In this example, we've overriden the rollover callback to show 'no date' for missing observations and have set <i>min_x</i> and <i>max_x</i> to dates beyond our date range.",
             data: data,
             interpolate: 'basic',
             missing_is_zero: true,
