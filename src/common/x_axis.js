e7eab9deb4e6dfecaa6ef16bb3470e06159145ab
hamilton <hamilton.ulmer@gmail.com>
2014-11-04 16:26:20 -0800

    fixes #176 - we were doing some weird padding thing and I can't remember why, but I removed it and now the year label doesn't collide with the date above it for very long graphics

--- src/common/x_axis.js
+++ src/common/x_axis.js
@@ -285,7 +285,7 @@ function x_axis(args) {
                 .append('text')
                     .attr('x', args.scales.X)
                     .attr('y', args.height - args.buffer + args.xax_tick_length)
-                    .attr('dy', args.use_small_class ? -3 : (args.y_extended_ticks) ? -6 : 0 )
+                    .attr('dy', args.use_small_class ? -3 : 0)//(args.y_extended_ticks) ? 0 : 0 )
                     .attr('text-anchor', 'middle')
                     .text(function(d) {
                         return yformat(d);
