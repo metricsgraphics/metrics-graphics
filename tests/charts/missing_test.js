368420422022ac2bbe1b7b7c2866e298ea50c7ef
almossawi <almossawi@gmail.com>
2015-01-14 14:43:28 -0800

    Unit tests for missing.js (#306)

---
+++ tests/charts/missing_test.js
@@ -0,0 +1,14 @@
+module('missing');
+
+test('Missing chart\'s text matches specified missing_text', function() {
+    var params = {
+        target: '#qunit-fixture',
+        chart_type: 'missing-data',
+        missing_text: 'In an astral plane that was never meant to fly...'
+    };
+
+    MG.data_graphic(params);
+    equal(document.querySelector('.mg-missing-text').innerHTML, 
+        params.missing_text, 
+        'Missing chart\'s text matches missing_text');
+});
