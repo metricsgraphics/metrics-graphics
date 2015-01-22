e95915b9b71dec763f3f6b7fd3b81dedcb22b7d2
Dan de Havilland <dan@dandehavilland.com>
2015-01-22 16:27:49 +0100

    jshint: redeclaration, performance - functions within loops

--- src/js/layout/button.js
+++ src/js/layout/button.js
@@ -51,13 +51,17 @@ MG.button_layout = function(target) {
 
         var d,f, features, feat;
         features = Object.keys(this.feature_set);
-        
+
+        var mapDtoF = function(f) { return d[f]; };
+
+        var i;
+
         // build out this.feature_set with this.data
-        for (var i=0; i < this._data.length; i++) {
+        for (i = 0; i < this._data.length; i++) {
             d = this._data[i];
-            f = features.map(function(f) { return d[f]; });
+            f = features.map(mapDtoF);
             for (var j = 0; j < features.length; j++) {
-                feat = features[j]; 
+                feat = features[j];
                 if (this.feature_set[feat].indexOf(f[j]) === -1) {
                     this.feature_set[feat].push(f[j]);
                 }
@@ -74,6 +78,21 @@ MG.button_layout = function(target) {
 
         $(this.target).append("<div class='col-lg-12 segments text-center'></div>");
 
+        var dropdownLiAClick = function() {
+            var k = $(this).data('key');
+            var feature = $(this).data('feature');
+            var manual_feature;
+            $('.' + feature + '-btns button.btn span.title').html(k);
+            if (!manual_map.hasOwnProperty(feature)) {
+                callback(feature, k);
+            } else {
+                manual_feature = manual_map[feature];
+                manual_callback[manual_feature](k);
+            }
+
+            return false;
+        };
+
         for (var feature in this.feature_set) {
             features = this.feature_set[feature];
             $(this.target + ' div.segments').append(
@@ -81,7 +100,7 @@ MG.button_layout = function(target) {
                     '<button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown">' +
                         "<span class='which-button'>" + (this.public_name.hasOwnProperty(feature) ? this.public_name[feature] : feature) +"</span>" +
                         "<span class='title'>" + (this.manual_callback.hasOwnProperty(feature) ? this.feature_set[feature][0] : 'all') +  "</span>" + // if a manual button, don't default to all in label.
-                        '<span class="caret"></span>' + 
+                        '<span class="caret"></span>' +
                     '</button>' +
                     '<ul class="dropdown-menu" role="menu">' +
                         (!this.manual_callback.hasOwnProperty(feature) ? '<li><a href="#" data-feature="'+feature+'" data-key="all">All</a></li>' : "") +
@@ -89,33 +108,20 @@ MG.button_layout = function(target) {
                     '</ul>'
             + '</div>');
 
-            for (var i = 0; i < features.length; i++) {
+            for (i = 0; i < features.length; i++) {
                 if (features[i] !== 'all' && features[i] !== undefined) { // strange bug with undefined being added to manual buttons.
                     $(this.target + ' div.' + this._strip_punctuation(feature) + '-btns ul.dropdown-menu').append(
-                        '<li><a href="#" data-feature="' + this._strip_punctuation(feature) + '" data-key="' + features[i] + '">' 
+                        '<li><a href="#" data-feature="' + this._strip_punctuation(feature) + '" data-key="' + features[i] + '">'
                             + features[i] + '</a></li>'
-                    ); 
+                    );
                 }
             }
 
-            $('.' + this._strip_punctuation(feature) + '-btns .dropdown-menu li a').on('click', function() {
-                var k = $(this).data('key'); 
-                var feature = $(this).data('feature');
-                var manual_feature;
-                $('.' + feature + '-btns button.btn span.title').html(k);
-                if (!manual_map.hasOwnProperty(feature)) {
-                    callback(feature, k);    
-                } else {
-                    manual_feature = manual_map[feature];
-                    manual_callback[manual_feature](k);
-                }
-                
-                return false;
-            });
+            $('.' + this._strip_punctuation(feature) + '-btns .dropdown-menu li a').on('click', dropdownLiAClick);
         }
 
         return this;
     };
 
-    return this
-}
+    return this;
+};
