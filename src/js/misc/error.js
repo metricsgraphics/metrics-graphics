481f09c97a3bb215b595b885c001c2411dcc6d09
Dan de Havilland <dan@dandehavilland.com>
2015-01-22 16:35:21 +0100

    jshint: redeclaration, reworking error message

--- src/js/misc/error.js
+++ src/js/misc/error.js
@@ -1,7 +1,8 @@
 //call this to add a warning icon to a graph and log an error to the console
 function error(args) {
-    var error = '<i class="fa fa-x fa-exclamation-circle warning"></i>';
     console.log('ERROR : ', args.target, ' : ', args.error);
-    
-    $(args.target).find('.mg-chart-title').append(error);
+
+    $(args.target)
+        .find('.mg-chart-title')
+        .append('<i class="fa fa-x fa-exclamation-circle warning"></i>');
 }
