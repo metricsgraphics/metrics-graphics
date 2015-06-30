7db7e4f42efa7c18dc3b2bcad9bb9fadee10c4e7
almossawi <almossawi@gmail.com>
2015-06-30 12:07:45 -0700

    Fix issue #423

--- dist/metricsgraphics.js
+++ dist/metricsgraphics.js
@@ -5268,6 +5268,7 @@ function isNumeric(n) {
 
 var each = function(obj, iterator, context) {
     // yanked out of underscore
+    var breaker = {};
     if (obj === null) return obj;
     if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
       obj.forEach(iterator, context);
