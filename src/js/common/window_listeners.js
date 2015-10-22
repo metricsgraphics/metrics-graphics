dadf13a44228b61de666cfdd25d8cfad7af0200a
almossawi <almossawi@gmail.com>
2015-10-22 13:16:53 -0700

    Cleanup

--- src/js/common/window_listeners.js
+++ src/js/common/window_listeners.js
@@ -1,11 +1,11 @@
-function mg_window_listeners(args){
+function mg_window_listeners(args) {
     mg_if_aspect_ratio_resize_svg(args);
 }
 
-function mg_if_aspect_ratio_resize_svg(args){
+function mg_if_aspect_ratio_resize_svg(args) {
     //have we asked the svg to fill a div, if so resize with div
-    if (args.full_width || args.full_height){
-        window.addEventListener('resize', function(){
+    if (args.full_width || args.full_height) {
+        window.addEventListener('resize', function() {
             var svg = d3.select(args.target).select('svg');
             var aspect = svg.attr('height') / svg.attr('width');
             var newWidth = get_width(args.target);
@@ -14,5 +14,4 @@ function mg_if_aspect_ratio_resize_svg(args){
             svg.attr('height', aspect * newWidth);
         }, true);
     }
-
 }
