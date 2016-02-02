4ef099a917ed14be8101229f01aecf03f8de6597
Ben Rabinovitch <github@ben.rabinovitch.ca>
2016-02-02 10:38:58 -0800

    Attach window resize handler in a removable way
    ref #544
    
    Add Function.prototype.bind polyfill to affected test suite in order to
    compensate for outdated PhantomJS

--- src/js/common/window_listeners.js
+++ src/js/common/window_listeners.js
@@ -1,3 +1,23 @@
+var bound_window_resize_listener;
+var that = this;
+
+var window_resize_listener = function window_resize_listener(args) {
+  var svg = d3.select(args.target).select('svg');
+
+  var aspect = svg.attr('width') !== 0
+      ? (svg.attr('height') / svg.attr('width'))
+      : 0;
+
+  var newWidth = get_width(args.target);
+
+  svg.attr('width', newWidth);
+  svg.attr('height', aspect * newWidth);
+};
+
+MG.remove_window_listeners = function mg_remove_window_listeners() {
+  window.removeEventListener('resize', bound_window_resize_listener, false);
+};
+
 function mg_window_listeners(args) {
   mg_if_aspect_ratio_resize_svg(args);
 }
@@ -5,21 +25,7 @@ function mg_window_listeners(args) {
 function mg_if_aspect_ratio_resize_svg(args) {
   // have we asked the svg to fill a div, if so resize with div
   if (args.full_width || args.full_height) {
-    if (window.onresize === null) {
-      window.onresize = window_listener;
-    }
-  }
-
-  function window_listener() {
-    var svg = d3.select(args.target).select('svg');
-
-    var aspect = svg.attr('width') !== 0
-        ? (svg.attr('height') / svg.attr('width'))
-        : 0;
-
-    var newWidth = get_width(args.target);
-
-    svg.attr('width', newWidth);
-    svg.attr('height', aspect * newWidth);
+    bound_window_resize_listener = window_resize_listener.bind(that, args);
+    window.addEventListener('resize', bound_window_resize_listener, false);
   }
 }
