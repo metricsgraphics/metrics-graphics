9f4f106fc0a51346da9d9fa970b4aa89138553d0
Jens Ox <github@jens-ox.de>
2020-02-29 20:40:16 +0100

    fix eslint, turn off esm because of preact issue, see preactjs/preact-cli/issues/984

--- packages/examples/src/routes/profile/index.js
+++ packages/examples/src/routes/profile/index.js
@@ -1,47 +1,47 @@
-import { h, Component } from 'preact';
-import style from './style';
+import { h, Component } from 'preact'
+import style from './style'
 
 export default class Profile extends Component {
-	state = {
-		time: Date.now(),
-		count: 10
-	};
+  state = {
+    time: Date.now(),
+    count: 10
+  };
 
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
+  // update the current time
+  updateTime = () => {
+    this.setState({ time: Date.now() })
+  };
 
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
+  increment = () => {
+    this.setState({ count: this.state.count + 1 })
+  };
 
-	// gets called when this route is navigated to
-	componentDidMount() {
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
+  // gets called when this route is navigated to
+  componentDidMount () {
+    // start a timer for the clock:
+    this.timer = setInterval(this.updateTime, 1000)
+  }
 
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
+  // gets called just before navigating away from the route
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
 
-	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
-		return (
-			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
+  // Note: `user` comes from the URL, courtesy of our router
+  render ({ user }, { time, count }) {
+    return (
+      <div class={style.profile}>
+        <h1>Profile: {user}</h1>
+        <p>This is the user profile for a user named {user}.</p>
 
-				<div>Current time: {new Date(time).toLocaleString()}</div>
+        <div>Current time: {new Date(time).toLocaleString()}</div>
 
-				<p>
-					<button onClick={this.increment}>Click Me</button>
-					{' '}
-					Clicked {count} times.
-				</p>
-			</div>
-		);
-	}
+        <p>
+          <button onClick={this.increment}>Click Me</button>
+          {' '}
+  Clicked {count} times.
+        </p>
+      </div>
+    )
+  }
 }
