ce87e43e7a5d057e4f8adb546d09cb3cf7dfc1e7
Jens Ox <github@jens-ox.de>
2020-02-29 21:40:40 +0100

    updated purgecss config, fingers crossed

--- packages/examples/preact.config.js
+++ packages/examples/preact.config.js
@@ -2,10 +2,8 @@
 module.exports = (config, env, helpers, params) => {
   const purgecss = require('@fullhuman/postcss-purgecss')({
     // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.js'],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(params.regex) || []
+    content: ['./build/**/*.js', './build/**/*.html'],
+    css: ['./build/**/*.css']
   })
 
   const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader')
@@ -16,7 +14,7 @@ module.exports = (config, env, helpers, params) => {
     plugins.unshift(require('tailwindcss'))
 
     // Add PurgeCSS only in production.
-    if (false /* env.production */) { // eslint-disable-line
+    if (env.production) {
       plugins.push(purgecss)
     }
   })
