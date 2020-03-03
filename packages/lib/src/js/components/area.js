9e3bcee6cd5ee6450c0c33f97cffc2f4fadc94f0
Jens Ox <github@jens-ox.de>
2020-03-04 00:26:44 +0100

    area: add props for more complex area limiters

--- packages/lib/src/js/components/area.js
+++ packages/lib/src/js/components/area.js
@@ -5,26 +5,34 @@ export default class Area {
   areaObject = null
   data = null
   index = 0
+  color = null
 
   constructor ({
     data,
     xAccessor,
     yAccessor,
+    y0Accessor,
+    y1Accessor,
     xScale,
     yScale,
     curve,
-    index
+    index,
+    color
   }) {
     // cry if no data was passed
     if (!data) throw new Error('line needs data')
     this.data = data
     this.index = index ?? this.index
+    this.color = color ?? schemeCategory10[this.index]
+
+    const y0 = y0Accessor ?? (d => 0)
+    const y1 = y1Accessor ?? yAccessor
 
     // set up line object
     this.areaObject = area()
       .x(d => xScale.scaleObject(xAccessor(d)))
-      .y1(d => yScale.scaleObject(yAccessor(d)))
-      .y0(d => yScale.scaleObject(0))
+      .y1(d => yScale.scaleObject(y1(d)))
+      .y0(d => yScale.scaleObject(y0(d)))
       .curve(curve ?? curveBasis)
   }
 
@@ -32,7 +40,7 @@ export default class Area {
     svg
       .append('path')
       .classed('mg-area', true)
-      .attr('fill', schemeCategory10[this.index])
+      .attr('fill', this.color)
       .datum(this.data)
       .attr('d', this.areaObject)
   }
