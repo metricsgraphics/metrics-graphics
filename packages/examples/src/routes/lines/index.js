3051fbfb235deb1411f89ad0aa26abad23d7e199
Jens Ox <github@jens-ox.de>
2020-03-04 00:27:52 +0100

    add confidence band example

--- packages/examples/src/routes/lines/index.js
+++ packages/examples/src/routes/lines/index.js
@@ -1,14 +1,16 @@
 import { Component } from 'preact'
 import { LineChart } from 'mg2'
 
-import data from '../../assets/data/fakeUsers1'
+import fakeUsers1 from '../../assets/data/fakeUsers1'
+import confidence from '../../assets/data/confidenceBand'
 
 export default class Lines extends Component {
   lineChart1 = null
+  confidenceBand = null
 
   componentDidMount () {
     this.lineChart1 = new LineChart({
-      data: data.map(entry => ({
+      data: fakeUsers1.map(entry => ({
         date: new Date(entry.date),
         value: entry.value
       })),
@@ -17,10 +19,27 @@ export default class Lines extends Component {
       yScale: {
         minValue: 0
       },
-      target: '#fake_users1',
+      target: '#fakeUsers1',
       area: true,
       xAccessor: 'date',
       yAccessor: 'value'
+    })
+
+    this.confidenceBand = new LineChart({
+      data: confidence.map(entry => ({
+        ...entry,
+        date: new Date(entry.date)
+      })),
+      xAxis: {
+        extendedTicks: true
+      },
+      yAxis: {
+        tickFormat: 'percentage'
+      },
+      width: 600,
+      height: 200,
+      target: '#confidenceBand',
+      confidenceBand: ['l', 'u']
     })
   }
 
@@ -28,13 +47,16 @@ export default class Lines extends Component {
   render () {
     return (
       <div>
-        <div className="flex">
-          <div className="flex-1 pr-2">
-            <p className="font-bold">Line Chart</p>
-            <p className="text-xs italic text-gray-700 mb-2">This is a simple line chart. You can remove the area portion by adding area: false to the arguments list.</p>
-            <div id="fake_users1" />
+
+        <div className="example-container">
+          <div>
+            <p>Line Chart</p>
+            <small>
+              This is a simple line chart. You can remove the area portion by adding area: false to the arguments list.
+            </small>
+            <div id="fakeUsers1" />
           </div>
-          <div className="flex-1 pl-2">
+          <div>
             <code><pre>{`new LineChart({
   data: data.map(entry => ({
     date: new Date(entry.date),
@@ -45,10 +67,38 @@ export default class Lines extends Component {
   yScale: {
     minValue: 0
   },
-  target: '#fake_users1',
+  target: '#fakeUsers1',
   area: true,
   xAccessor: 'date',
   yAccessor: 'value'
+})`}</pre></code>
+          </div>
+        </div>
+
+        <div className="example-container">
+          <div>
+            <p>Confidence Band</p>
+            <small>
+              This is an example of a graphic with a confidence band and extended x-axis ticks enabled.
+            </small>
+            <div id="confidenceBand" />
+          </div>
+          <div>
+            <code><pre>{`new LineChart({
+  data: confidence.map(entry => ({
+    ...entry,
+    date: new Date(entry.date)
+  })),
+  xAxis: {
+    extendedTicks: true
+  },
+  yAxis: {
+    tickFormat: 'percentage'
+  },
+  width: 600,
+  height: 200,
+  target: '#confidenceBand',
+  confidenceBand: ['l', 'u']
 })`}</pre></code>
           </div>
         </div>
