3ddbe878c17bf6f5278316a7f36019b5a5e7f516
Jens Ochsenmeier <github@jens-ox.de>
2020-03-29 20:10:14 +0200

    add multi-dimensional scatter chart

--- packages/examples/src/routes/scatter.js
+++ packages/examples/src/routes/scatter.js
@@ -3,17 +3,43 @@ import ScatterChart from 'mg2/src/js/charts/scatter'
 
 import points1 from '../assets/data/points1'
 
+const groupByArray = (xs, key) => xs.reduce((rv, x) => {
+  let v = key instanceof Function ? key(x) : x[key]
+  let el = rv.find((r) => r && r.key === v)
+  if (el) el.values.push(x)
+  else rv.push({ key: v, values: [x] })
+  return rv
+}, [])
+const points2 = groupByArray(points1, 'v')
+
+const Num = new Intl.NumberFormat('en', {
+  maximumFractionDigits: 2
+})
+
 export default class Lines extends Component {
   scatterChart1 = null
+  scatterChart2 = null
 
   componentDidMount () {
     this.scatterChart1 = new ScatterChart({
       data: points1,
-      width: 300,
+      width: 500,
       height: 200,
       target: '#scatterChart1',
       xAccessor: 'x',
-      yAccessor: 'y'
+      yAccessor: 'y',
+      tooltipFunction: point => `${Num.format(point.x)} ${Num.format(point.y)}`
+    })
+    this.scatterChart2 = new ScatterChart({
+      data: points2.map(x => x.values),
+      legend: points2.map(x => x.key),
+      width: 500,
+      height: 200,
+      target: '#scatterChart2',
+      xAccessor: 'x',
+      yAccessor: 'y',
+      tooltipFunction: point => `${Num.format(point.x)} ${Num.format(point.y)}`,
+      legendTarget: '#scatterChart2Legend'
     })
   }
 
@@ -27,6 +53,20 @@ export default class Lines extends Component {
             This is an example scatterplot, in which we have enabled rug plots on the y-axis by setting the rug option to true.
             </small>
             <div id="scatterChart1" />
+          </div>
+          <div>
+            <code><pre /></code>
+          </div>
+        </div>
+
+        <div className="example-container">
+          <div>
+            <p>Multi-Category Scatterplot</p>
+            <small>
+            This scatterplot contains data of multiple categories.
+            </small>
+            <div id="scatterChart2" />
+            <p className="text-center" id="scatterChart2Legend" />
           </div>
           <div>
             <code><pre /></code>
