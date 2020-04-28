05e2b8a8807208e601841b4428a64121e71cc370
Jens Ox <github@jens-ox.de>
2020-04-28 12:38:48 +0200

    clean up gulpfile

--- packages/lib/gulpfile.js
+++ packages/lib/gulpfile.js
@@ -1,14 +1,6 @@
-const { join } = require('path')
-const { src, dest, watch, series, parallel } = require('gulp')
-const umd = require('gulp-umd')
+const { src, dest, watch } = require('gulp')
 
-const rimraf = require('gulp-rimraf')
-const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
-const rename = require('gulp-rename')
 const eslint = require('gulp-eslint')
-const { reload, server } = require('gulp-connect')
-const babel = require('gulp-babel')
 
 const sass = require('gulp-sass')
 sass.compiler = require('node-sass')
@@ -17,31 +9,6 @@ sass.compiler = require('node-sass')
 const distFolder = 'dist'
 const jsFiles = 'src/js/**/*'
 const sassFiles = 'src/sass/**/*'
-
-const clean = () => {
-  return src(join(distFolder, '*'), { read: false })
-    .pipe(rimraf())
-}
-
-const buildJsRaw = () => {
-  return src(jsFiles)
-    .pipe(concat({ path: 'metricsgraphics.js' }))
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(umd(
-      {
-        exports: () => 'MG',
-        namespace: () => 'MG'
-      }
-    ))
-    .pipe(dest(distFolder))
-    .pipe(rename('metricsgraphics.min.js'))
-    .pipe(uglify())
-    .pipe(dest(distFolder))
-}
-
-const buildJs = series(clean, buildJsRaw)
 
 const lint = () => {
   return src(jsFiles)
@@ -56,21 +23,6 @@ const compileSass = () => src(sassFiles)
 
 const watchSass = () => watch(sassFiles, compileSass)
 
-const roots = ['dist', 'examples']
-const watchables = roots.map(root => `${root}/**/*`)
-
-const devReload = () => src(watchables).pipe(reload())
-const devWatch = () => parallel(watch(watchables, devReload), watch(sassFiles, compileSass))
-
-const devServe = () => server({
-  root: roots,
-  port: 4300,
-  livereload: true
-})
-
-exports.clean = clean
-exports.default = series(lint, buildJs, compileSass)
-exports.serve = series(compileSass, devServe, devWatch)
 exports.lint = lint
 exports.compileSass = compileSass
 exports.watchSass = watchSass
