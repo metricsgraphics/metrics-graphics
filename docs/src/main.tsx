ff5d5a3b28022dc2d1eda515a2574202bd9b5aa9
Jens Ochsenmeier <github@jens-ox.de>
2022-05-19 17:40:21 +0200

    switch to HashRouter

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import 'metrics-graphics/dist/mg.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
